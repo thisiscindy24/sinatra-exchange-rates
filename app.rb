0e39dccef2fa56cb4aaf14269a61c82c91ea8ae3
thisiscindy24 <cindyannwu@gmail.com>
2025-02-06 07:36:06 +0000

    symbols

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "dotenv/load"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  exchange_key = ENV.fetch("EXCHANGE_KEY")
+  exchange_url = "https://api.exchangerate.host/list?access_key=#{exchange_key}"
+  @raw_response = HTTP.get(exchange_url)
+  @parsed_response = JSON.parse(@raw_response)
+  @currencies = @parsed_response.fetch("currencies")
+  erb(:homepage)
 end
