7b6609c0ec0377cee75b7f8ed0a62ad7f3ab13b2
thisiscindy24 <cindyannwu@gmail.com>
2025-02-06 08:30:38 +0000

    final

--- app.rb
+++ app.rb
@@ -14,6 +14,28 @@ get("/") do
 end
 
 get("/:first_symbol") do
-  @symbol = params.fetch("first_symbol")
+  @the_symbol = params.fetch("first_symbol")
+  exchange_key = ENV.fetch("EXCHANGE_KEY")
+  exchange_url = "https://api.exchangerate.host/list?access_key=#{exchange_key}"
+  @raw_response = HTTP.get(exchange_url)
+  @parsed_response = JSON.parse(@raw_response)
+  @currencies = @parsed_response.fetch("currencies")
   erb(:step_one)
+end
+
+get ("/:first_symbol/:second_symbol") do
+  @first_symbol = params.fetch("first_symbol")
+  @second_symbol = params.fetch("second_symbol")
+
+  exchange_key = ENV.fetch("EXCHANGE_KEY")
+  convert_url = "https://api.exchangerate.host/convert?from=#{@first_symbol}&to=#{@second_symbol}&amount=1&access_key=#{exchange_key}"
+  @raw_response = HTTP.get(convert_url)
+  @parsed_response = JSON.parse(@raw_response)
+  @amount = @parsed_response.fetch("result")
+
+ 
+
+
+  erb(:step_two)
+  
 end
